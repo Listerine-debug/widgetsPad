09c216021dac531a2dcd17410453e5910a89c0e1
Listerine-debug <warriordogg1@gmail.com>
2026-03-14 11:30:30 -0400

    Update creator name in AppMain.cpp

--- src/AppMain.cpp
+++ src/AppMain.cpp
@@ -1,5 +1,5 @@
 // File Name: AppMain.cpp
-// Creator: Matteo Washington (Aka: Listerine-debug)
+// Creator: (Aka: Listerine-debug)
 // Date of last modification: July 11 2025
 // Copyright (c) 2025 Matteo Washington 
 // Description: main execute 
